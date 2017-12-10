981f88426955093a8b02caba8784382d06240cf2
zcb973 <1187088687@qq.com>
2017-12-10 14:15:42 +0800

    add test for hacking liststring1

---
+++ Hack/test_list_print.py
@@ -0,0 +1,25 @@
+#!/home/cbzhang/Sourcecode/Python/python
+
+list1 = [1]
+print "list1: ", list1
+
+list2 = [1]
+print "list2: ", list2
+
+list3 = [1]
+print "list3: ", list3
+
+list4 = [1]
+print "list4: ", list4
+
+list5 = [1]
+print "list5: ", list5
+
+list6 = [1]
+print "list6: ", list6
+
+del list3
+print "after delete list3, list1: ", list1
+
+list7 = [1]
+print "list7: ", list7
